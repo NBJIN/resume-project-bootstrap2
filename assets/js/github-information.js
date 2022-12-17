50f6731d4ce318de7f9ba66e9ac68d187268cae6
noreen <noreennelligan1@gmail.com>
2022-12-17 18:07:08 +0000

    fix bugs for empty divs

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -35,6 +35,9 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
+
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -68,4 +71,6 @@ function fetchGitHubInformation(event) {
                 }
             });
             }
+
+            $(document).ready(fetchGitHubInformation);
         
